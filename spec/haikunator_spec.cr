a369f4007b8041faa4dfd4c93f50340a95c357de
Dave Takahashi <dtcello@gmail.com>
2016-08-19 14:41:24 -0700

    Add tests

--- spec/haikunator_spec.cr
+++ spec/haikunator_spec.cr
@@ -1,7 +1,40 @@
 require "./spec_helper"
 
 describe Haikunator do
-  it "works" do
-    false.should eq(false)
+  it "generates a name like still-silence-5012" do
+    name = Haikunator.haikunate
+
+    name.should match(/\A\w+-\w+-\d{1,4}\z/)
+  end
+
+  it "won't return the same name for subsequent calls" do
+    name1 = Haikunator.haikunate
+    name2 = Haikunator.haikunate
+
+    name1.should_not eq(name2)
+  end
+
+  it "permits optional configuration of the token range" do
+    name = Haikunator.haikunate(9)
+
+    name.should match(/-\d{1}\z/)
+  end
+
+  it "drops the token if token range is 0" do
+    name = Haikunator.haikunate(0)
+
+    name.should match(/\A\w+-\w+\z/)
+  end
+
+  it "permits optional configuration of the delimiter" do
+    name = Haikunator.haikunate(9999, ".")
+
+    name.should match(/\A\w+\.\w+\.\d{1,4}\z/)
+  end
+
+  it "drops the token and delimiter if token range is 0 and delimiter empty space" do
+    name = Haikunator.haikunate(0, " ")
+
+    name.should match(/\A\w+ \w+\z/)
   end
 end
